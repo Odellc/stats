ce5fad52ad213de6fb29a3e41badb5bf051447b0
Chris Odell <codell03@gmail.com>
2022-10-18 15:24:33 -0600

    update Factorial MANOVA

--- Factorial_MANOVA_and_Assumptions.R
+++ Factorial_MANOVA_and_Assumptions.R
@@ -23,3 +23,37 @@ Manova(soils.mod, multivariate=TRUE, type=c("II"), test=("Wilks"))
 Manova(soils.mod, multivariate=TRUE, type=c("II"), test=("Pillai"))
 Manova(soils.mod, multivariate=TRUE, type=c("II"), test=("Hotelling-Lawley"))
 Manova(soils.mod, multivariate=TRUE, type=c("II"), test=("Roy"))
+
+
+#Eta Squared for Contour
+#Effects of Size (Partial Eta-Squared Values)
+
+wilks_lamda = 0.00939 #From the summary of the wilks manova
+
+#S = min(P, df_effect)
+min(3, 3)
+
+#Partial Eta squared = 1 - wilks_lamnda ^ 1/s
+
+1-(0.00939)^(1/3)
+
+
+#78.9% of variance of the pH, Density, and Conductivity of the soil are due to
+#to the differences in the Contour in the soil
+
+
+
+#Eta Squared for Depth
+#Effects of Size (Partial Eta-Squared Values)
+
+wilks_lamda = 0.03164 #From the summary of the wilks manova
+
+#S = min(P, df_effect)
+min(3, 3)
+
+#Partial Eta squared = 1 - wilks_lamnda ^ 1/s
+
+1-(0.03164)^(1/3)
+
+#68.37% of variance of the pH, Density, and Conductivity of the soil are due to
+#to the differences in the Depth in the soil
