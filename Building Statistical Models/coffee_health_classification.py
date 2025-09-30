1273ab859fbc475a427dfa0e09f155f62f6862b2
Odellc <codell03@gmail.com>
2025-09-30 16:26:26 -0700

    Adding in more Shap plots, for bar, waterfall, explanations, and force plots to better understand why certain observations are classified into a particular category

--- Building Statistical Models/coffee_health_classification.py
+++ Building Statistical Models/coffee_health_classification.py
@@ -4,6 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt 
 from sklearn.preprocessing import StandardScaler
+import sys
+
+#Shap imports
+import shap 
 
 
 def main():
@@ -33,6 +37,14 @@ def main():
     test_accuracies = {}
 
     n_range = np.arange(1, 26)
+
+    best_model = {
+        'model': '',
+        'fitted_model' : '',
+        'test_accuracy' : 0,
+        'gap' : 0
+        }
+    
     
     for neighbor in n_range:
         
@@ -41,7 +53,17 @@ def main():
 
         train_accuracies[neighbor] = knn.score(X_train_scaled, y_train)
         test_accuracies[neighbor] = knn.score(X_test_scaled, y_test)
+
+        if best_model['model'] == '' or best_model['test_accuracy'] <= knn.score(X_test_scaled, y_test):
+            if (best_model['test_accuracy'] == test_accuracies[neighbor]) and (train_accuracies[neighbor] - test_accuracies[neighbor] >= best_model['gap']):
+                continue
+            
+            best_model['model'] = knn
+            best_model['fitted_model'] = best_model['model'].fit(X_train_scaled, y_train)
+            best_model['test_accuracy'] = knn.score(X_test_scaled, y_test)
+            best_model['gap'] = train_accuracies[neighbor] - test_accuracies[neighbor]
     
+    print(best_model)
 
     plt.figure(figsize=(8,6))
     plt.title('KNN varying number of neighbors')
@@ -52,10 +74,55 @@ def main():
     plt.ylabel('Accuracy')
     plt.show()
 
+    # sys.exit()
+
+    #Run model to determine shap
+    # knn = KNeighborsClassifier(n_neighbors=13)
+    # knn.fit(X_train_scaled, y_train)
+
+    # print(X_train_scaled)
+
+    explainer = shap.KernelExplainer(best_model['model'].predict_proba, X_train_scaled)
+    shap_values = explainer.shap_values(X_test_scaled)
+
+    print(type(shap_values))
+    print(len(shap_values))
+    print(shap_values.shape)
+    print(shap_values[0].shape)
+    print(shap_values[1].shape)
+
+    # Choose class index (e.g., class 1)
+    class_index = 1
+
+    # Extract SHAP values for class 1
+    shap_values_class1 = shap_values[:, :, class_index]  # shape: (2000, 2)
+
+    col = ['Caffeine_mg', 'Sleep_Hours']
+
+    # Plot
+    shap.summary_plot(shap_values_class1, pd.DataFrame(X_test_scaled, columns=col))
+
+    # shap.initjs()
+
+    # shap.force_plot(explainer.expected_value, shap_values_class1[0], X_test_scaled[0], feature_names=col)
+    shap.plots.force(explainer.expected_value[0], shap_values_class1[0], X_test_scaled[0], feature_names=col, matplotlib=True, show=True)
+
+    explanation = shap.Explanation(
+    values=shap_values_class1[0],
+    base_values=explainer.expected_value[0],
+    data=X_test_scaled[0],
+    feature_names=col
+    )
+
+    # Now plot
+    shap.plots.bar(explanation)
+    shap.plots.waterfall(explanation)
 
 
+    # shap.plots.bar(explainer.expected_value[0], shap_values_class1[0], X_test_scaled[0])
 
-    
+    # shap.plots.waterfall(explainer.expected_value[0], shap_values_class1[0], X_test_scaled[0])
+
 
 
 if __name__ == '__main__':
